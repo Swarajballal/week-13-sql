bf6ff7588c3678587b86c12bf281267eca7868cc
Swaraj Ballal <swaraj.ballal@vit.edu.in>
2023-09-03 18:31:07 +0530

    Week 13

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("postgres://vdtpuqeq:6a_N5HTHqp1Fn_Nd0_LrVsqxfY6VfWYb@rajje.db.elephantsql.com/vdtpuqeq");
     await client.connect();
     return client;
 }
